9d372c11e8ec8e34534a70e4699879869515a51c
Jerico Rodriguez <j.rodriguez.enz@gmail.com>
2024-10-01 14:54:59 +0200

    PROGRESSA-1: Defined correctly urls

--- src/app/progressa/shared/constants/progressa.api.constants.ts
+++ src/app/progressa/shared/constants/progressa.api.constants.ts
@@ -1,4 +1,3 @@
-// TODO define correctly api_url for create workout, exercise and set
 export const WORKOUT_URL = 'http://localhost:8080/workout';
 export const EXERCISE_URL = 'http://localhost:8080/exercise';
 export const SET_URL = 'http://localhost:8080/set';
