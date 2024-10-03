6d713113481ea4bd7eae857e63d56dd875e01381
Jerico Rodriguez <j.rodriguez.enz@gmail.com>
2024-10-03 13:53:20 +0200

    PROGRESSA-1: Modified constants urls

--- src/app/progressa/shared/constants/progressa.api.constants.ts
+++ src/app/progressa/shared/constants/progressa.api.constants.ts
@@ -1,3 +1,3 @@
 export const WORKOUT_URL = 'http://localhost:8080/workout';
 export const EXERCISE_URL = 'http://localhost:8080/exercise';
-export const SET_URL = 'http://localhost:8080/set';
+export const EXERCISE_SET_URL = 'http://localhost:8080/set';
