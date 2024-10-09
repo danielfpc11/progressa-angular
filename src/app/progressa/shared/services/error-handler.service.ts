60456c06d5b3565d2a09dd20a8aee29016636b77
Jerico Rodriguez <j.rodriguez.enz@gmail.com>
2024-10-09 19:33:30 +0200

    PROGRESSA-1: Added error documentation

--- src/app/progressa/shared/services/error-handler.service.ts
+++ src/app/progressa/shared/services/error-handler.service.ts
@@ -5,9 +5,14 @@ import { Observable, throwError } from 'rxjs';
   providedIn: 'root'
 })
 export class ErrorHandlerService {
-
+  /**
+   * Logs the error and returns an observable that throws the error.
+   *
+   * @param {Error} error - The error object to be handled.
+   * @returns {Observable<never>} - An observable that throws a new error.
+   */
   handleError(error: Error): Observable<never> {
-    console.error(`Error fetching data: ${ error.message }`);
+    console.error(`Error fetching data: ${error.message}`);
     return throwError(() => new Error(error.message));
   }
 }
